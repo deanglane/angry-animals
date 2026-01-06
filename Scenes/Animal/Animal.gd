e20b53eb1d9688c5fb2876b8de7caf68b378c7c6
deanglane <dglaneto@gmail.com>
2026-01-06 07:04:50 -0500

    Added logic for flight and arrow scale/rotation

--- Scenes/Animal/Animal.gd
+++ Scenes/Animal/Animal.gd
@@ -7,6 +7,8 @@ enum AnimalState { Ready, Drag, Release }
 
 const DRAG_LIM_MAX: Vector2 = Vector2(0, 60) # Bottom right
 const DRAG_LIM_MIN: Vector2 = Vector2(-60, 0)
+const IMPLUSE_MULTI: float = 20.0 # multiplier for the  fire impulse (needs better comment)
+const IMPLUSE_MAX: float = 1200.00 # max value for the impulse (needs better comment)
 
 
 @onready var arrow: Sprite2D = $Arrow
@@ -23,22 +25,22 @@ var _arrorw_scale_x: float = 0.0 # scale from the start of the arrow
 
 func _unhandled_input(event: InputEvent) -> void:
 	if _state == AnimalState.Drag and event.is_action_released("drag"):
-		change_state(AnimalState.Release)
-		
+		call_deferred("change_state", AnimalState.Release) # defers the call up to the end of the current frame
 
 # Called when the node enters the scene tree for the first time.
+# Get the game setup and ready to play
 func _ready() -> void:
 	setup()
 
 func setup() -> void:
-	_arrorw_scale_x = arrow.scale.x
+	_arrorw_scale_x = arrow.scale.x # Stores the scale.x of the arrow for strecthing later
 	print("_arrorw_scale_x ",_arrorw_scale_x)
 	arrow.hide() # hides the arrow sprte when the game starts
-	_start = position
-	print("_start ",_start)
+	_start = position # stores the initial position of the animal sprite
+	print("_start (initial starting point) ",_start)
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	update_state()
 	update_debug_label()
 
@@ -58,6 +60,17 @@ func update_debug_label() -> void:
 
 # DRAGGING FUNCTIONS
 #region drag
+
+# Handles the linear interpolation of the dragging and scalling effect of the arrow
+func update_arrow_scale() -> void:
+	var imp_len: float = calculate_impulse().length() # stores the length of the vector
+	print("imp_len ",imp_len)
+	var perc: float = clamp(imp_len / IMPLUSE_MAX, 0.0, 1.0) # creates a percentage value which is the length / max impulse (1200)
+	print("perc ",perc)
+	arrow.scale.x = lerp(_arrorw_scale_x, _arrorw_scale_x * 2, perc) # Changes the scale of on the x axis using lerp which sets a value between the 2 _arrorw_scale_x(0.3) & (0.6) values based on the percentage.
+	print("arrow.scale.x ",arrow.scale.x)
+	arrow.rotation = (_start - position).angle()
+	print("arrow.rotation ",_start - position)
 
 # This will show the arrow sprite and set the gloabl mouse position to the _drag_start varibale.
 func start_dragging() -> void:
@@ -91,17 +104,23 @@ func handle_dragging() -> void:
 	print("START", _start) # original click global position
 	position = _start + _dragged_vector # update the animal's position every frame so it looks like it is moving with the mouse cursor
 	print("position ",position)
+	
+	update_arrow_scale() # evokes the stretching and scalling of the arrow
 
 #endregion
 
 #region release
+
+# function to calculate the impulse force
+func calculate_impulse() -> Vector2:
+	return _dragged_vector * -IMPLUSE_MULTI
 
 # what to do when the click has been released.
 func start_release() -> void:
 	arrow.hide() # hides the srrow upon release of the mouse click
 	launch_sound.play() # plays the launch sound fx 
 	freeze = false # unfreezes the ridgidBody2d sprite
-	
+	apply_central_impulse(calculate_impulse())
 
 #endregion
 
@@ -131,7 +150,7 @@ func change_state(new_state: AnimalState) -> void:
 
 # ALL SIGNAL FUNCTIONS
 #region signals
-func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	# Check to see if the mouse buttong has been clicked and the currect state is set to Ready.
 	if event.is_action_pressed("drag") and _state == AnimalState.Ready:
 		change_state(AnimalState.Drag) # Changes the currect state to Drag. We pass the state Drag to the function
@@ -145,7 +164,7 @@ func _on_sleeping_state_changed() -> void:
 	pass # Replace with function body.
 
 
-func _on_body_entered(body: Node) -> void:
+func _on_body_entered(_body: Node) -> void:
 	pass # Replace with function body.
 
 #endregion
